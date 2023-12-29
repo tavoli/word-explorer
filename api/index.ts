5c93f046de064843c97a2d92732eff74cc42f061
Gustavo Lima <gustavolima@lima.local>
2023-12-29 20:42:47 -0300

    chore: hide api keys

--- api/index.ts
+++ api/index.ts
@@ -2,9 +2,6 @@ export const config = {
   runtime: 'edge',
 };
  
-const when = '2023-12-26T17:11:44.687Z'
-const key = '8cfdb189e723919be89707bee858bfb8aeb4290936f790b8'
-
 const headers = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET',
@@ -13,7 +10,7 @@ const headers = {
 }
 
 async function fetchWordsapi(w: string) {
-  return fetch(`https://www.wordsapi.com/mashape/words/${w}?when=${when}&encrypted=${key}`)
+  return fetch(`https://www.wordsapi.com/mashape/words/${w}?when=2023-12-26T17:11:44.687Z&encrypted=${process.env.API_KEY}`)
 }
 
 export default async function handler(request: Request) {
